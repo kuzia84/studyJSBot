f7da404f30380ae13f5766d8cc1fa3eae06949e2
kuzia84 <45180124+kuzia84@users.noreply.github.com>
2020-10-15 19:35:44 +0300

    difficult bot fix

--- script.js
+++ script.js
@@ -2,70 +2,73 @@
 
 //функция для обработки условий игры
 let start = function () {
-  //функция для случайного числа
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-  //бот загадывает число от 1 до 100
-  const botNumber = getRandomInt(100) + 1;
-  console.log("botNumber: ", botNumber);
-
-  const maxTry = 3;
-  let currentTry = 0;
-  //функция ввода данных пользователя
-  function enterData(userNumber) {
-    //объявляем переменную для чисела пользователя
-    userNumber = prompt("Введи число от 1 до 100");
-
-    //условие для отмены игры
-    if (userNumber === null) {
-      alert("Игра окончена!");
-      return;
-    }
-    //функция проверки данных пользователя
-    function checkData() {
-      //делаем из строки число
-      userNumber = parseFloat(userNumber);
-
-      //если не получилось сделать строку, ввыодим сообщение и запрашиваем данные заново
-      if (isNaN(userNumber)) {
-        alert("Введите число");
-        enterData();
-      }
+    //функция для случайного числа
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
 
-    checkData();
+    //бот загадывает число от 1 до 100
+    const botNumber = getRandomInt(100) + 1;
+    console.log("botNumber: ", botNumber);
 
-    // сравнение числа бота и числа пользователя
+    const maxTry = 3;
+    let currentTry = 0;
 
-    if (userNumber > botNumber) {
-      alert(`Загаданное число меньше, осталось попыток ${maxTry - currentTry}`);
-      enterData();
-    } else if (userNumber < botNumber) {
-      alert(`Загаданное число больше, осталось попыток ${maxTry - currentTry}`);
-      enterData();
-    } else {
-      let oneMore = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-      if (oneMore) {
-        start();
-      } else {
-        return;
-      }
+    //функция ввода данных пользователя
+    function enterData(userNumber) {
+        //объявляем переменную для чисела пользователя
+        userNumber = prompt("Введи число от 1 до 100");
+
+        //условие для отмены игры
+        if (userNumber === null) {
+            alert("Игра окончена!");
+            return;
+        }
+        //функция проверки данных пользователя
+        function checkData() {
+            //делаем из строки число
+            userNumber = parseFloat(userNumber);
+
+            //если не получилось сделать строку, ввыодим сообщение и запрашиваем данные заново
+            if (isNaN(userNumber)) {
+                alert("Введите число");
+                enterData();
+            }
+        }
+
+        checkData();
+
+        //проверка оставшихся попыток
+        if (currentTry === maxTry) {
+            let oneMoreTime = confirm("Попытки закончились, хотите сыграть еще?");
+            if (oneMoreTime) {
+                start();
+            } else {
+                return;
+            }
+        }
+
+        // сравнение числа бота и числа пользователя
+
+        if (userNumber > botNumber) {
+            alert(`Загаданное число меньше, осталось попыток ${maxTry - currentTry}`);
+            currentTry++;
+            enterData();
+        } else if (userNumber < botNumber) {
+            alert(`Загаданное число больше, осталось попыток ${maxTry - currentTry}`);
+            currentTry++;
+            enterData();
+        } else {
+            let oneMore = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+            if (oneMore) {
+                start();
+            } else {
+                return;
+            }
+        }
+
     }
-
-    currentTry++;
-
-    if (currentTry === maxTry) {
-      let oneMoreTime = confirm("Попытки закончились, хотите сыграть еще?");
-      if (oneMoreTime) {
-        start();
-      } else {
-        return;
-      }
-    }
-  }
-  enterData();
+    enterData();
 };
 //игра начинается!!!
 start();
