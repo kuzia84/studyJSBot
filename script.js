03d0c4ee8dcaf263d3a74a59a619e078bf0cedd1
kuzia84 <45180124+kuzia84@users.noreply.github.com>
2020-10-15 16:36:00 +0300

    Bot Fix

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
 "use strict";
 
-//функция для случайного числа
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-//бот загадывает число от 1 до 100
-const botNumber = getRandomInt(100) + 1;
-console.log("botNumber: ", botNumber);
-
-//функция для сравнения числа бота и числа пользователя
-function chechNumber(botNumber, userNumber) {
-  if (userNumber > botNumber) {
-    alert("Загаданное число меньше");
-    start();
-  } else if (userNumber < botNumber) {
-    alert("Загаданное число больше");
-    start();
-  } else {
-    alert("Поздравляю, Вы угадали!!!");
-    return;
-  }
-}
-
 //функция для обработки условий игры
 let start = function () {
-  //объявляем переменную для чисела пользователя
-  let userNumber = prompt("Введи число от 1 до 100");
-
-  //условие для отмены игры
-  if (userNumber === null) {
-    alert("Игра окончена!");
-    return;
+  //функция для случайного числа
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
-  //делаем из строки число
-  userNumber = parseFloat(userNumber);
+  //бот загадывает число от 1 до 100
+  const botNumber = getRandomInt(100) + 1;
+  console.log("botNumber: ", botNumber);
 
-  //если не получилось сделать строку, ввыодим сообщение и запрашиваем данные заново
-  if (isNaN(userNumber)) {
-    alert("Введите число");
-    start();
+  //функция ввода данных пользователя
+  function enterData(userNumber) {
+    //объявляем переменную для чисела пользователя
+    userNumber = prompt("Введи число от 1 до 100");
+
+    //условие для отмены игры
+    if (userNumber === null) {
+      alert("Игра окончена!");
+      return;
+    }
+    //функция проверки данных пользователя
+    function checkData() {
+      //делаем из строки число
+      userNumber = parseFloat(userNumber);
+
+      //если не получилось сделать строку, ввыодим сообщение и запрашиваем данные заново
+      if (isNaN(userNumber)) {
+        alert("Введите число");
+        enterData();
+      }
+    }
+
+    checkData();
+
+    // сравнение числа бота и числа пользователя
+
+    if (userNumber > botNumber) {
+      alert(`Загаданное число меньше`);
+      enterData();
+    } else if (userNumber < botNumber) {
+      alert(`Загаданное число больше`);
+      enterData();
+    } else {
+      alert("Поздравляю, Вы угадали!!!");
+      return;
+    }
   }
-
-  //вызываем функцию сравнения чисел
-  chechNumber(botNumber, userNumber);
+  enterData();
 };
 //игра начинается!!!
 start();
